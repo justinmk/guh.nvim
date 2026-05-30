d7c7ac01ba5cd08613fec6903f106ca3b166f166
Justin M. Keyes <justinkz@gmail.com>
2026-05-30 15:12:51 +0200

    feat: ENTER mapping in guh:// buffers

--- lua/guh/init.lua
+++ lua/guh/init.lua
@@ -1,19 +1,31 @@
 local config = require('guh.config')
-local pr_commands = require('guh.pr_commands')
 
 local M = {}
 
 M.setup = function(user_config)
   config.setup(user_config)
 
-  vim.api.nvim_create_user_command('Guh', pr_commands.select, { nargs = '?' })
-  vim.api.nvim_create_user_command('GuhDiff', pr_commands.show_pr_diff, { nargs = '?' })
-  vim.api.nvim_create_user_command('GuhComment', pr_commands.comment, { bang = true, range = true })
-  vim.api.nvim_create_user_command('GuhLoadComments', pr_commands.load_comments, { nargs = '?' })
-  -- vim.api.nvim_create_user_command('GuhCheckout', pr_commands.checkout, { nargs = '?' })
-  -- vim.api.nvim_create_user_command('GuhApprove', pr_commands.approve_pr, {})
-  -- vim.api.nvim_create_user_command('GuhRequestChanges', pr_commands.request_changes_pr, {})
-  -- vim.api.nvim_create_user_command('GuhMerge', pr_commands.merge_pr, {})
+  vim.api.nvim_create_autocmd('BufFilePost', {
+    pattern = 'guh://*',
+    group = vim.api.nvim_create_augroup('guh.keymaps', { clear = true }),
+    callback = function(args)
+      vim.keymap.set('n', '<CR>', function()
+        local text = vim.fn.expand('<cWORD>')
+        if require('guh.util').parse_target(text) then
+          vim.cmd('Guh ' .. text)
+        end
+      end, { buffer = args.buf, desc = 'Open :Guh target at cursor' })
+    end,
+  })
+
+  vim.api.nvim_create_user_command('Guh', require('guh.pr_commands').select, { nargs = '?' })
+  vim.api.nvim_create_user_command('GuhDiff', require('guh.pr_commands').show_pr_diff, { nargs = '?' })
+  vim.api.nvim_create_user_command('GuhComment', require('guh.pr_commands').comment, { bang = true, range = true })
+  vim.api.nvim_create_user_command('GuhLoadComments', require('guh.pr_commands').load_comments, { nargs = '?' })
+  -- vim.api.nvim_create_user_command('GuhCheckout', require('guh.pr_commands').checkout, { nargs = '?' })
+  -- vim.api.nvim_create_user_command('GuhApprove', require('guh.pr_commands').approve_pr, {})
+  -- vim.api.nvim_create_user_command('GuhRequestChanges', require('guh.pr_commands').request_changes_pr, {})
+  -- vim.api.nvim_create_user_command('GuhMerge', require('guh.pr_commands').merge_pr, {})
   -- vim.api.nvim_create_user_command('GuhCommentEdit', comments.update_comment, { range = true })
   -- vim.api.nvim_create_user_command('GuhCommentDelete', comments.delete_comment, { range = true })
 end
